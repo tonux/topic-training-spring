08be9cc8ec26f73a70180f2758cc6990ec034f6b
Médoune Siby Georges Baldé <40875400+MedouneSGB@users.noreply.github.com>
2023-04-21 07:30:54 +0000

    Add findByEmail

--- src/main/java/com/workshop/testapp/repositories/UserRepository.java
+++ src/main/java/com/workshop/testapp/repositories/UserRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByPhone(String phone);
+    User findByEmail(String email);
 }
